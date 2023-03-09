e47ee031c0c341745abbcda07d93f8815d2dfeef
Gustavo Soares <gustavohvs.dev@gmail.com>
2023-03-09 16:46:58 -0300

    função validator

--- impetus/utils/ImpetusUtils.php
+++ impetus/utils/ImpetusUtils.php
@@ -62,16 +62,150 @@ class ImpetusUtils
     }
 
     /**
+     * isShortString
+     */
+    static function isShortString($string, $limit)
+    {
+        if (strlen($string) < $limit) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
      * isNumber
      */
     static public function isNumber($number)
     {
         if (is_numeric($number)) {
-        $response = true;
+            $response = true;
         } else {
-        $response = false;
+            $response = false;
         }
         return $response;
+    }
+
+    /**
+     * isInt
+     */
+    static public function isInt($number)
+    {
+        if (is_int($number)) {
+            $response = true;
+        } else {
+            $response = false;
+        }
+        return $response;
+    }
+
+    /**
+     * enum
+     */
+    static function enum($string, $list)
+    {
+        $found = false;
+        foreach($list as $item){
+            if($item == $string){
+                $found = true;
+            }
+        }
+        return $found;
+    }
+
+    /**
+     * isDate
+     */
+    static function isDate($string)
+    {
+        $isDate = false;
+        $data = \DateTime::createFromFormat('d/m/Y', $string);
+        if($data && $data->format('d/m/Y') === $string){
+           $isDate = true;
+        }
+        return $isDate;
+    }
+
+    /**
+     * isDateTime
+     */
+    static function isDateTime($string)
+    {
+        $isDate = false;
+        $data = \DateTime::createFromFormat('d/m/Y H:i:s', $string);
+        if($data && $data->format('d/m/Y H:i:s') === $string){
+           $isDate = true;
+        }
+        return $isDate;
+    }
+
+    /**
+     * isEmail
+     */
+    static function isEmail($string)
+    {
+        if(filter_var($string, FILTER_VALIDATE_EMAIL)) {
+            $isEmail = true;
+        }else{
+            $isEmail = false;
+        }
+        return $isEmail;
+    }
+
+    /**
+     * isBoolean
+     */
+    static function isBoolean($string)
+    {
+        if($string === 1 || $string === 0 || $string === true || $string === false) {
+            $isBoolean = true;
+        }else{
+            $isBoolean = false;
+        }
+        return $isBoolean;
+    }
+
+    /**
+     * isSecurePassword
+     */
+    static public function isSecurePassword($string) 
+    {
+        $isSecurePassword = true;
+
+        if (strlen($string) < 8) {
+            $isSecurePassword = false;
+        }
+        if (!preg_match("#[0-9]+#", $string)) {
+            $isSecurePassword = false;
+        }
+        if (!preg_match("#[a-zA-Z]+#", $string)) {
+            $isSecurePassword = false;
+        }     
+    
+        return $isSecurePassword;
+    }
+
+    /**
+     * isStrongPassword
+     */
+    static public function isStrongPassword($string) 
+    {
+        $isSecurePassword = true;
+
+        if (strlen($string) < 8) {
+            $isSecurePassword = false;
+        }
+        if (!preg_match("#[0-9]+#", $string)) {
+            $isSecurePassword = false;
+        }
+        if (!preg_match("#[a-zA-Z]+#", $string)) {
+            $isSecurePassword = false;
+        }   
+        if (preg_match('/^[a-zA-Z0-9]+/', $string)) {
+            $isSecurePassword = false;
+        }  
+    
+        return $isSecurePassword;
     }
 
     /**
@@ -100,6 +234,220 @@ class ImpetusUtils
         }
         return $string;
     }
+
+    /**
+     * validator
+     */
+    static function validator($name, $request, $config)
+    {
+        //config [type(date), lenght(15), enum(list), specialChar, uppercase, nullable]
+
+        //Parâmetros
+        $typeParam = false;
+        $nullableParam = false;
+        $lengthParam = false;
+        $enumParam = false;
+        $specialCharParam = false;
+        $uppercaseParam = false;
+        $lowercaseParam = false;
+
+        //Verificar parâmetros passados
+        foreach($config as $param){
+            $paramString = explode("(", $param);
+            $paramName = $paramString[0];
+            if(isset($paramString[1])){
+                $paramString = explode(")", $paramString[1]);
+                $paramValue = $paramString[0];
+            }else{
+                $paramValue = null;
+            }
+            if($paramName == "type"){
+                $typeParam = true;
+                $typeParamValue = $paramValue;
+            }
+            if($paramName == "nullable"){
+                $nullableParam = true;
+            }
+            if($paramName == "length"){
+                $lengthParam = true;
+                $lengthParamValue = explode("-", $paramValue);
+                if(isset($lengthParamValue[1])){
+                    $lengthParamMinValue = $lengthParamValue[0];
+                    $lengthParamMaxValue = $lengthParamValue[1];
+                }else{
+                    $lengthParamMinValue = 0;
+                    $lengthParamMaxValue = $lengthParamValue[0];
+                }
+            }
+            if($paramName == "enum"){
+                $enumParam = true;
+                $enumParamValue = $paramValue;
+            }
+            if($paramName == "specialChar"){
+                $specialCharParam = true;
+            }
+            if($paramName == "uppercase"){
+                $uppercaseParam = true;
+            }
+            if($paramName == "lowercase"){
+                $lowercaseParam = true;
+            }
+        }
+
+        //Tratar variável
+        if($typeParamValue == "string"){
+            $string = trim($request);
+            $string = stripcslashes($string);
+            $string = htmlspecialchars($string);
+        }else{
+            $string = $request;
+        }
+
+        //Realizar verificações
+        if($nullableParam == false && ($typeParamValue != 'boolean' && $typeParamValue != 'number' && $typeParamValue != 'int')){
+            $validate = ImpetusUtils::isEmpty($string);
+            if($validate == true){
+                return [
+                    "status" => 0,
+                    "info" => "Campo '" . $name . "' não pode ser vazio."
+                ];
+            }
+        }
+
+        if($lengthParam == true){
+            $validate = ImpetusUtils::isLongString($string, $lengthParamMaxValue);
+            if($validate == true){
+                return [
+                    "status" => 0,
+                    "info" => "Campo '" . $name . "' excede quantidade máxima de caracteres (" . $lengthParamMaxValue . ")"
+                ];
+            }
+            $validate = ImpetusUtils::isShortString($string, $lengthParamMinValue);
+            if($validate == true){
+                return [
+                    "status" => 0,
+                    "info" => "Campo '" . $name . "' não possui caracteres suficientes (" . $lengthParamMinValue . ")"
+                ];
+            }
+        }
+
+        if($typeParam == true){
+            if($typeParamValue == "number"){
+                $validate = ImpetusUtils::isNumber($string);
+                if($validate == false){
+                    return [
+                        "status" => 0,
+                        "info" => "Campo '" . $name . "' não é um número válido."
+                    ];
+                }
+            }elseif($typeParamValue == "date"){
+                $validate = ImpetusUtils::isDate($string);
+                if($validate == false){
+                    return [
+                        "status" => 0,
+                        "info" => "Campo '" . $name . "' não é uma data válida."
+                    ];
+                }
+            }elseif($typeParamValue == "int"){
+                $validate = ImpetusUtils::isInt($string);
+                if($validate == false){
+                    return [
+                        "status" => 0,
+                        "info" => "Campo '" . $name . "' não é um número inteiro."
+                    ];
+                }
+            }elseif($typeParamValue == "email"){
+                $validate = ImpetusUtils::isEmail($string);
+                if($validate == false){
+                    return [
+                        "status" => 0,
+                        "info" => "Campo '" . $name . "' não é um email válido."
+                    ];
+                }
+            }elseif($typeParamValue == "boolean"){
+                $validate = ImpetusUtils::isBoolean($string);
+                if($validate == false){
+                    return [
+                        "status" => 0,
+                        "info" => "Campo '" . $name . "' não é um valor booleano."
+                    ];
+                }
+            }elseif($typeParamValue == "datetime"){
+                $validate = ImpetusUtils::isDateTime($string);
+                if($validate == false){
+                    return [
+                        "status" => 0,
+                        "info" => "Campo '" . $name . "' não é um valor datetime (d/m/Y H:i:s)."
+                    ];
+                }
+            }elseif($typeParamValue == "password"){
+                $validate = ImpetusUtils::isSecurePassword($string);
+                if($validate == false){
+                    return [
+                        "status" => 0,
+                        "info" => "Campo '" . $name . "' não é uma senha segura. Deve conter mais que 8 caracteres, ao menos uma letra e um número."
+                    ];
+                }
+            }elseif($typeParamValue == "strongPassword"){
+                $validate = ImpetusUtils::isStrongPassword($string);
+                if($validate == false){
+                    return [
+                        "status" => 0,
+                        "info" => "Campo '" . $name . "' não é uma senha segura. Deve conter mais que 8 caracteres, ao menos uma letra, um número e um caracter especial."
+                    ];
+                }
+            }elseif($typeParamValue == "string"){
+                //Do nothing
+            }else{
+                return [
+                    "status" => 0,
+                    "info" => "Tipo de variável não identificado"
+                ];
+            }
+        }
+
+        if($specialCharParam == false && $typeParamValue=="string"){
+            $string = preg_replace(array("/(á|à|ã|â|ä)/", "/(Á|À|Ã|Â|Ä)/", "/(é|è|ê|ë)/", "/(É|È|Ê|Ë)/", "/(í|ì|î|ï)/", "/(Í|Ì|Î|Ï)/", "/(ó|ò|õ|ô|ö)/", "/(Ó|Ò|Õ|Ô|Ö)/", "/(ú|ù|û|ü)/", "/(Ú|Ù|Û|Ü)/", "/(ñ)/", "/(Ñ)/"), explode(" ", "a A e E i I o O u U n N"), $string);
+            $string = preg_replace('/[^a-z0-9 ]/i', '', $string);
+        }
+
+        if($uppercaseParam == true && $typeParamValue=="string"){
+            $string = strtoupper($string);
+        }
+
+        if($lowercaseParam == true && $typeParamValue=="string"){
+            $string = strtolower($string);
+        }
+
+        if($enumParam == true){
+            $validate = false;
+            $list = explode("|", $enumParamValue);
+            $itens = "";
+            $separador = "";
+            foreach($list as $item){
+                $itens .= $separador . $item;
+                $separador = "|";
+                if($item == $string){
+                    $validate = true;
+                }
+            }
+            if($validate == false){
+                return [
+                    "status" => 0,
+                    "info" => "Valor '" . $name . "' não disponível nas opções (Opções disponíveis: " . $itens . ")"
+                ];
+            }
+        }
+
+        //Retornar string editada
+        return [
+            "status" => 1,
+            "info" => "Todos as regras validadas com sucesso",
+            "data" => $string
+        ];
+
+    }
+
 
     /**
      * base64UrlEncode
@@ -144,4 +492,8 @@ class ImpetusUtils
         }
         return null;
     }
+
 }
+
+//$validate = ImpetusUtils::validator("var", "@Ab123456", ['type(strongPassword)']);
+//var_dump($validate);
