755300bed45d43163ce782a6cd9db573725cff23
Gustavo Soares <gustavohvs.dev@gmail.com>
2024-03-07 11:39:53 -0300

    fix validação nullable

--- impetusSDK/cmd/snippets/backend/app/utils/ImpetusUtils.php
+++ impetusSDK/cmd/snippets/backend/app/utils/ImpetusUtils.php
@@ -397,7 +397,7 @@ class ImpetusUtils
         }
 
         //Realizar verificações
-        if($nullableParam == false && ($typeParamValue != 'boolean' && $typeParamValue != 'number' && $typeParamValue != 'int')){
+        if($nullableParam == false && $string === null){
             $validate = ImpetusUtils::isEmpty($string);
             if($validate == true){
                 return [
