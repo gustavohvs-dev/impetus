04f1f725ce159828eed1a50e411453fd79f4a92f
Rafael Penido Rocha <rafaelpenidorocha.19@gmail.com>
2024-06-27 17:19:50 -0300

    Testes unitários em JWTTest

--- impetusSDK/projects/ws/tests/unit/ImpetusJWTTest.php
+++ impetusSDK/projects/ws/tests/unit/ImpetusJWTTest.php
@@ -6,13 +6,36 @@ use PHPUnit\Framework\TestCase;
 class JWTTest extends TestCase
 {
 
-    public function testCreateAndValidateToken()
+    public function test_create_and_validate_token()
     {
         $ImpetusJWT = new ImpetusJWT;
         $jwt = $ImpetusJWT->encode("1", "username", ["id" => 1, "username" => "username"], 24, "#SECRETKey12318209");
         $decode = $ImpetusJWT->decode($jwt, "#SECRETKey12318209");
         $this->assertEquals("1", $decode->payload->id);
         $this->assertEquals("username", $decode->payload->username);
+    }
+
+    public function test_invalid_token()
+    {
+        $ImpetusJWT = new ImpetusJWT;
+        $decode = $ImpetusJWT->decode("invalid_token", "#SECRETKey12318209");
+        $this->assertEquals("0", $decode->status);
+    }
+
+    public function test_expired_token()
+    {
+        $ImpetusJWT = new ImpetusJWT;
+        $jwt = $ImpetusJWT->encode("1", "username", ["id" => 1, "username" => "username"], -1, "#SECRETKey12318209");
+        $decode = $ImpetusJWT->decode($jwt, "#SECRETKey12318209");
+        $this->assertEquals("0", $decode->status);
+    }
+
+    public function test_wrong_secret_token()
+    {
+        $ImpetusJWT = new ImpetusJWT;
+        $jwt = $ImpetusJWT->encode("1", "username", ["id" => 1, "username" => "username"], -1, "#SECRETKey12318209");
+        $decode = $ImpetusJWT->decode($jwt, "@SUPERSecretKey1093824$$#@!@#@#!%$!");
+        $this->assertEquals("0", $decode->status);
     }
 
 }
