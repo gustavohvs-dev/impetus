8f15d847c58ec97eaa4c7697b81de9af3241689d
Rafael Penido Rocha <rafaelpenidorocha.19@gmail.com>
2024-06-27 11:52:56 -0300

    Impetus JWT Test

---
+++ impetusSDK/projects/ws/tests/unit/ImpetusJWTTest.php
@@ -0,0 +1,18 @@
+<?php
+
+use app\utils\ImpetusJWT;
+use PHPUnit\Framework\TestCase;
+
+class UtilsTest extends TestCase
+{
+
+    public function testCreateAndValidateToken()
+    {
+        $ImpetusJWT = new ImpetusJWT;
+        $jwt = $ImpetusJWT->encode("1", "username", ["id" => 1, "username" => "username"], 24, "#SECRETKey12318209");
+        $decode = $ImpetusJWT->decode($jwt, "#SECRETKey12318209");
+        $this->assertEquals("1", $decode->payload->id);
+        $this->assertEquals("username", $decode->payload->username);
+    }
+
+}
